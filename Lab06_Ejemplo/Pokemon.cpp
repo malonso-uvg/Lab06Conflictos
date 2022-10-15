18ab62abcd004a8a723e1506f408cdf8f84a5d6a
Moises Alonso <maalonso@uvg.edu.gt>
2022-10-15 09:59:48 -0600

    Correccion de la UI, ahora con descripcion del pokemon

--- Lab06_Ejemplo/Pokemon.cpp
+++ Lab06_Ejemplo/Pokemon.cpp
@@ -1,9 +1,25 @@
 #include "Pokemon.h"
 
 String^ Pokemon::getName() {
-	return pokemonName;
+	return name;
 }
 
 String^ Pokemon::getGeneration() {
-	return this->generacion;
+	return this->generation;
+}
+
+String^ Pokemon::getType() {
+	return tipo;
+}
+
+void Pokemon::setName(String^ _name) {
+	this->name = _name;
+}
+
+void Pokemon::setGeneration(String^ _generation) {
+	this->generation = _generation;
+}
+
+void Pokemon::setType(String^ _type) {
+	this->tipo = _type;
 }
